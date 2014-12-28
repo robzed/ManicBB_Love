d5d8ffaaf6e5d4f568fd226844e6fe63d84ac9a3
Rob Probin <rob.probin@gmail.com>
2014-12-28 18:28:47 +0000

    Added details of license.

--- main.lua
+++ main.lua
@@ -1,6 +1,22 @@
--- Converter for Andy Nobel's Blitz Basic Manic Miner to Love2D
+-- Converter for Andy Noble's Blitz Basic Manic Miner to Love2D
 -- by Rob Probin, Dec 2014
 -- Copyright © 2014 Rob Probin
+--[[
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details. 
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+--]]
+
 
 local manic_miner_url = "http://retrospec.sgn.net/download.php?link=mm&url=http://www.retrospec.sgn.net/users/anoble/files/ManicBB.zip"
 local mmf = "ManicBB.zip"
--- utilities/bb_parser.lua
+++ utilities/bb_parser.lua
@@ -1,5 +1,20 @@
--- Blitz Basic Parser for Manic Miner
+-- Blitz Basic Parser for Manic Miner code by Andy Noble.
 -- Copyright © 2014 Rob Probin
+--[[
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details. 
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+--]]
 
 class = require("utilities/middleclass")
 
